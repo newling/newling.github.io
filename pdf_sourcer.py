e59cc2b6d1cb689c1608be0230482fdc3a06eac7
James Newling <james.newling@gmail.com>
2017-11-27 18:23:50 +0100

    get public pdfs from their generating dirs

---
+++ pdf_sourcer.py
@@ -0,0 +1,27 @@
+import commands
+import os
+import socket
+
+filenames = [
+"posters/2017/nips/clarans/clarans_poster.pdf", 
+"slides/2017/nips/clarans/clarans_spotlight.pdf",
+"slides/2017/smld/clarans/clarans_smld.pdf",
+"cv_4.pdf"
+]
+
+if socket.gethostname() == "goudurix11":
+  fetchpaths = {}
+  for fn in filenames:
+    fetchpaths[fn] = os.path.join("../idiap-ftex", fn)
+  fetchpaths["cv_4.pdf"] = "../cv/cv_4.pdf"
+
+else:
+  raise RuntimeWarning("Unrecognised hostname : ", socket.gethostname())
+  fetchpaths = {}
+  for fn in filenames:
+    fetchpaths[fn] = "where/should/this/pdf/be/sourced/from/?"
+    
+
+for fn in fetchpaths.keys():
+  print "sourcing %s"%(fn,) 
+  commands.getstatusoutput("cp %s %s "%(fetchpaths[fn], fn))

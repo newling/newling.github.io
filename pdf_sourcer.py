12108bdcd08d867307c09b2fbf32394eab79bc34
James Newling <james.newling@gmail.com>
2017-11-27 21:19:30 +0100

    add missing page number

--- pdf_sourcer.py
+++ pdf_sourcer.py
@@ -3,17 +3,18 @@ import os
 import socket
 
 filenames = [
-"posters/2017/nips/clarans/clarans_poster.pdf", 
-"slides/2017/nips/clarans/clarans_spotlight.pdf",
+#"posters/2017/nips/clarans/clarans_poster.pdf", 
+#"slides/2017/nips/clarans/clarans_spotlight.pdf",
 "slides/2017/smld/clarans/newling_smld.pdf",
-"cv_4.pdf"
+#"cv_4.pdf"
 ]
 
-if socket.gethostname() == "goudurix11":
+if socket.gethostname() == "goudurix11" or "idbean":
   fetchpaths = {}
   for fn in filenames:
     fetchpaths[fn] = os.path.join("../idiap-ftex", fn)
   fetchpaths["cv_4.pdf"] = "../cv/cv_4.pdf"
+
 
 else:
   raise RuntimeWarning("Unrecognised hostname : ", socket.gethostname())
@@ -22,6 +23,6 @@ else:
     fetchpaths[fn] = "where/should/this/pdf/be/sourced/from/?"
     
 
-for fn in fetchpaths.keys():
+for fn in filenames: 
   print "sourcing %s"%(fn,) 
   commands.getstatusoutput("cp %s %s "%(fetchpaths[fn], fn))
